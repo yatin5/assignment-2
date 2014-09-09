1a56b7724528345d7992922be86b0a38beeb8a0c
Len Payne <len.payne@lambtoncollege.ca>
2014-09-09 12:30:07 -0400

    Initial Commit - Problems Outlined

---
+++ src/assign2/pkg2014f/Assign22014F.java
@@ -0,0 +1,128 @@
+package assign2.pkg2014f;
+
+import java.util.Scanner;
+
+/**
+ * @author <ENTER YOUR NAME HERE>
+ */
+public class Assign22014F {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* =============================== *
+         * DO NOT CHANGE THIS METHOD       *
+         * Make changes in the sub-methods *
+         * named for each exercise.        *
+         * =============================== */
+        int choice;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.println("Which exercise would you like to run? [4,9,10,12,16] (0 to Exit)");
+            choice = in.nextInt();
+            switch (choice) {
+                case 0:
+                    System.out.println("Okay, goodbye!");
+                    break;
+                case 4:
+                    doExercise4();
+                    break;
+                case 9:
+                    doExercise9();
+                    break;
+                case 10:
+                    doExercise10();
+                    break;
+                case 12:
+                    doExercise12();
+                    break;
+                case 16:
+                    doExercise16();
+                    break;
+                default:
+                    System.out.println("That is not a valid choice.");
+                    break;
+            }
+        } while (choice != 0);
+    }
+
+    /* Exercise #4
+     * Write a program that displays the following pattern:
+     *     *
+     *    ***
+     *   *****
+     *  *******
+     *   *****
+     *    ***
+     *     *
+     */
+    public static void doExercise4() {
+        // TODO: Complete Exercise #4 Below
+
+    }
+
+    /* Exercise #9
+     * A car's miles-per-gallon (MPG) can be calculated 
+     * with the following formula:
+     *   MPG = Miles Driven / Gallons of Gas Used
+     * Write a program that asks the user for the number 
+     * of miles driven and the gallons of gas used. It 
+     * should calculate the car's miles-per-gallon and 
+     * display the result on the screen.
+     */
+    public static void doExercise9() {
+        // TODO: Complete Exercise #9 Below
+
+    }
+
+    /* Exercise #10
+     * Write a program that asks the user to enter three 
+     * test scores. The program should display each test 
+     * score, as well as the average of the scores.
+     */
+    public static void doExercise10() {
+        // TODO: Complete Exercise #10 Below
+
+    }
+
+    /* Exercise #12
+     * Write a program that asks the user to enter the name 
+     * of his or her favourite city. Use a String variable 
+     * to store the input. The program should display the 
+     * following:
+     * - The number of characters in the city name
+     * - The name of the city in all uppercase letters
+     * - The name of the city in all lowercase letters
+     * - The first character in the name of the city
+     */
+    public static void doExercise12() {
+        // TODO: Complete Exercise #12 Below
+
+    }
+
+    /* Exercise #16
+     * Write a program that plays a word game with the 
+     * user. The program should ask the user to enter 
+     * the following:
+     * - His or her name
+     * - His or her age
+     * - The name of a city
+     * - The name of a college
+     * - A profession
+     * - A type of animal
+     * - A pet's name
+     * After the user has entered these items, the program 
+     * should display the following story, inserting the 
+     * user's input into appropriate locations:
+     *
+     * There once was a person named NAME who lived in CITY. At the age of AGE,
+     * NAME went to college at COLLEGE. NAME graduated and went to work as a
+     * PROFESSION. Then, NAME adopted a(n) ANIMAL named PETNAME. They both
+     * lived happily ever after.
+     */
+    public static void doExercise16() {
+        // TODO: Complete Exercise #16 Below
+
+    }
+}
